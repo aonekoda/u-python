74ea640524cf1415b014a9e108384fb042b8d15b
aonekoda <51275765+aonekoda@users.noreply.github.com>
2021-02-22 21:54:41 +0900

    Magic Method, Polymorphism

---
+++ MagicShoppingCart.py
@@ -0,0 +1,9 @@
+class MagicShoppingCart:
+    def __init__(self, items):
+        self.items = items
+    def __len__(self):
+        return sum(self.items.values())
+    def __str__(self):
+        return f"MagicShoppingCart({self.items})"
+    def __contains__(self, item):
+        return item in self.items
